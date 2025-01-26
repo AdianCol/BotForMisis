86b45873284220348883cff41be9204924cb375e
AdianCol <156536098+AdianCol@users.noreply.github.com>
2025-01-27 00:23:17 +0300

    Add files via upload

--- Bot — копия.py
+++ Bot — копия.py
@@ -20,11 +20,11 @@ logger = logging.getLogger(__name__)
 def connect_db():
     try:
         conn = psycopg2.connect(
-            dbname=os.environ.get("Notes"),
-            user=os.environ.get("postgres"),
-            password=os.environ.get("FKQgOrWZQDdtnIDHcySBmsAhKUxPaNKs"),
-            host=os.environ.get("postgres.railway.internal"),
-            port=os.environ.get("5432")
+            dbname=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            host=os.environ.get("DB_HOST.railway.internal"),
+            port=os.environ.get("DB_PORT")
         )
         return conn
     except Exception as e:
